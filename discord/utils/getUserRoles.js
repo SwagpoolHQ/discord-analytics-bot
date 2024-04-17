b92ac308280fa47c109b0418a5348d8342632fc8
ArthurOnTime <112695921+ArthurOnTime@users.noreply.github.com>
2024-04-17 19:34:52 +0200

    fix getUserRoles crash
    Happened when roles array are not instantiated because object missing from cache

--- discord/utils/getUserRoles.js
+++ discord/utils/getUserRoles.js
@@ -1,27 +1,28 @@
 import discordClient from "../../discord/index.js";
 
-export default async function getUserRoles( guildId , userId ) {
+export default async function getUserRoles(guildId, userId) {
 
     // Check DiscordJS beginners guide => https://anidiots.guide/understanding/roles/
 
-    if ( !guildId || !userId ){
-        throw new Error(`Parameter required => guildId: ${ !!guildId ? '✅' : '❌' }, userId: ${ !!userId ? '✅' : '❌' }.`)
+    if (!guildId || !userId) {
+        throw new Error(`Parameter required => guildId: ${!!guildId ? '✅' : '❌'}, userId: ${!!userId ? '✅' : '❌'}.`)
     }
 
     const client = await discordClient()
-    
-    const guild = client.guilds.cache.get( guildId );
-    const member = guild?.members.cache.get( userId)
-    
-    let roles = member?.roles.cache ;
-    if ( !roles ){
+
+    const guild = client.guilds.cache.get(guildId);
+    const member = guild?.members.cache.get(userId)
+
+    let roles = member?.roles.cache;
+    let userRoles = [];
+    if (!roles) {
         // get role by ID
         //roles = guild.roles.cache.get( userId );
         //roles = await member.roles.fetch( userId );
+    } else {
+        userRoles = roles.map(item => item.name);
     }
 
-    const userRoles = roles.map( item => item.name );
-    
     // get role by name
     //let myRole = message.guild.roles.cache.find(role => role.name === "Moderators");
 
