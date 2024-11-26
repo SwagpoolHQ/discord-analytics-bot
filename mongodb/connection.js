fe859a868226938aa1d2654ff67754cf5cbc8061
ArthurOnTime <112695921+ArthurOnTime@users.noreply.github.com>
2024-11-26 15:08:27 +0100

    micro fix

--- mongodb/connection.js
+++ mongodb/connection.js
@@ -2,16 +2,16 @@ import mongodb from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log('connecting to Mongo DB - contribution tracker. VPN break the connection')
+console.log('connecting to Mongo DB - Discordlinks. VPN break the connection')
 
 const connectionString = process.env.MONGO_DB_CONNEXION
 
 // ready states being: 0: disconnected - 1: connected - 2: connecting - 3: disconnecting
-if (mongodb.connection.readyState === 1){
+if (mongodb.connection.readyState === 1) {
     console.log('Mongo DB is already connected with status : ', mongodb.connection.readyState);
 } else {
     // handle connection
-    mongodb.connect( connectionString, {connectTimeoutMS:2000})
-    .then(() => console.log('Mongo DB - contribution tracker is connected'))
-    .catch((error) => console.log(error))
+    mongodb.connect(connectionString, { connectTimeoutMS: 2000 })
+        .then(() => console.log('Mongo DB - Discordlinks is connected'))
+        .catch((error) => console.log(error))
 }
